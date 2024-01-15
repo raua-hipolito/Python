3d9f683edec5b465d585486d7de45aa3607f2a93
Rauã <rauavasconcelos22@gmail.com>
2024-01-15 08:47:27 -0300

    ✅ Olá Mundoo com python e iniciando operadores Aritméticos

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,14 @@
+# Opereções básicas com operadores aritméticos
+# soma, subtração, multiplicação, divisão
+
+# Declaração de variáveis
+soma = 10 + 10
+subtracao = 10 - 5
+multiplicacao = 10 * 10
+divisao = 10 / 2
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(divisao)
+
+print(soma + subtracao + multiplicacao + divisao)
