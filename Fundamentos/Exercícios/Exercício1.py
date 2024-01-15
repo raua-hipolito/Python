0852beb8db897606b63411dbd7268a7208afe808
Rauã <rauavasconcelos22@gmail.com>
2024-01-15 10:59:34 -0300

    ⚡️ criando pasta e arquivos para exercicios ,vqv

---
+++ Fundamentos/Exercícios/Exercício1.py
@@ -0,0 +1,18 @@
+# inicialize duas variáveis a e b, sendo a = 10 e b = 5. Mostre o
+# resultado das 7 operações básicas (soma, subtração, multiplicação, divisão,
+# divisão inteira, potenciação e módulo) envolvendo essas variáveis.
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# Declare e inicialize uma variável: hours = 6. Quantos minutos têm em 6 horas?
+# E quantos segundos? Declare e inicialize variáveis minutes e seconds que
+# os respectivos resultados das contas. Depois, imprima cada uma delas.
+
+
+# ------------------------------------------------------------------------------------------------------------------------
+
+# Suponha que o preço de capa de um livro seja R$ 24,20, mas as livrarias
+# recebem um desconto de 40%. O transporte custa 3,00 para o primeiro exemplar
+# e 75 centavos para cada exemplar adicional. Qual é o custo total de atacado
+# para 60 cópias? Escreva uma expressão que receba o custo total e a imprima.
