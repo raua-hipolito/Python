128736444febff75b0721ca89aba41a351a30949
Rauã <rauavasconcelos22@gmail.com>
2024-01-17 10:47:52 -0300

    🐛 resolvendo um bug com pytest no arquivo main e resolvendo um exercicio no arquivo exercice

---
+++ tests_python/pytest_bug/exercice_test.py
@@ -0,0 +1,13 @@
+# Exercício 1
+# A função sum_two_numbers abaixo contém um bug. Crie um exemplo na docstring
+# que pega esse bug ao rodar o módulo doctest e, em seguida, corrija-o.
+
+def sum_two_numbers(a, b):
+    """Retorna a soma de dois números recebidos por parâmetro.
+
+    Exemplos
+    --------
+    >>> sum_two_numbers(1, 4)
+    5
+    """
+    return a + b
